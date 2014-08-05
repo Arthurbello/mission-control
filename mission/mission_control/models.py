80ac27ce7f742bb777fd20e528d84da88bcc0cf4
kremerdesign <kremerdesign@gmail.com>
2014-08-05 15:42:46 -0700

    models and migration

--- mission/mission_control/models.py
+++ mission/mission_control/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=120)
+    week = models.IntegerField()
+    DAY_CHOICES = (
+        ("1", "Monday"),
+        ("2", "Tuesday"),
+        ("3", "Wednesday"),
+        ("4", "Thursday"),
+        ("5", "Friday"),
+    )
+    day = models.CharField(max_length=20, choices=DAY_CHOICES)
+    AMPM_CHOICES = (
+        ("AM", "AM"),
+        ("PM", "PM"),
+    )
+    am_pm = models.CharField(max_length=10, choices=AMPM_CHOICES)
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.ForeignKey(Topic, related_name='exercise')
+    difficulty = models.CharField(max_length=120)
