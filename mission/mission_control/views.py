10517f9b65bb53b04bea3693ac8d8e30e7e32854
Tad <tad@whitakerresearch.com>
2014-08-05 16:35:36 -0700

    working on views

--- mission/mission_control/views.py
+++ mission/mission_control/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 
+
 def register(request):
     if request.method == 'POST':
-        form = EmailUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             # user.email_user("Welcome!", "Thank you for signing up for our website.")
             text_content = 'Thank you for signing up for our website, {}'.format(user.first_name)
             html_content = '<h2>Thanks {} for signing up!</h2> <div>I hope you enjoy using our site. You registered on {}.</div>'.format(user.first_name, user.date_joined)
-            msg = EmailMultiAlternatives("Welcome!", text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
+            msg = MultiAlternatives("Welcome!", text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
             msg.attach_alternative(html_content, "text/html")
             msg.send()
             return redirect("profile")
     else:
-        form = EmailUserCreationForm()
+        form = UserCreationForm()
 
     return render(request, "registration/register.html", {
         'form': form,
