70231b1048035468c1de3318f0c64fb9b9351f8d
Armen Suny <armenlsuny@gmail.com>
2014-08-05 15:43:26 -0700

    I moves some folders around

--- mission/mission_control/views.py
+++ mission/mission_control/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# # Create your views here.
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             return redirect("home")
+#     else:
+#         form = UserCreationForm()
+#     # this will most likely need to be pointed elsewhere
+#     return render(request, "registration/register", {"form": form})
+#
+#
+# def logout_view(request):
+#     logout(request)
+#
+#
+# @login_required
+# def home(request):
+#   return HttpResponse('Home Page')
+
