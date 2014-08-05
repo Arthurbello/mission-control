8274fedc40b115b42e9963ee8a5797d6165a6684
Tad <tad@whitakerresearch.com>
2014-08-05 15:49:26 -0700

    image, urls, views

--- mission/mission/urls.py
+++ mission/mission/urls.py
@@ -9,4 +9,19 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'mission_control.views.home', name='home'),
+    url(r'^register/$', 'mission_control.views.register', name='register'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logged_out/$', 'django.contrib.auth.views.logged_out', name='logged_out'),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    # Support old style base36 password reset links; remove in Django 1.7
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm_uidb36'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+
+
 )
